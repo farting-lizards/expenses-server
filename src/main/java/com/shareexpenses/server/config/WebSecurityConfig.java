2769d1f819cb95b10d6d40f605ab4f20c23cc33b
Dinika Saxena <dinikasaxenas@gmail.com>
2022-12-03 12:18:02 +0100

    Disable CORS and any authentication for local development

--- src/main/java/com/shareexpenses/server/config/WebSecurityConfig.java
+++ src/main/java/com/shareexpenses/server/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.shareexpenses.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!local")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
