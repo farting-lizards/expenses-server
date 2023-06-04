b237f502d0e01ebd12cad72364521d8740498a16
David Caro <me@dcaro.es>
2023-06-04 17:58:40 +0200

    Add dev dockerfile and start script
    Signed-off-by: David Caro <me@dcaro.es>

---
+++ scripts/start_dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+hash podman && DOCKER="sudo podman" || DOCKER=docker
+
+
+
+$DOCKER build -f Dockerfile.dev -t expenses-server:dev
+$DOCKER run \
+    --tty \
+    --interactive \
+    --volume $PWD:/src:rw,idmap \
+    --volume $PWD/../home-lab-secrets/:/home-lab-secrets:rw,idmap \
+    --publish-all \
+    --rm \
+    expenses-server:dev
